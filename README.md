8df3040b47961526f11413f995e2f72a225735b2
shen-sir <674735426@qq.com>
2016-12-05 08:38:28 +0800

    first

---
+++ README.md
@@ -0,0 +1,18 @@
+# pig_pc_web
+
+> 猪队友PC官方  vue.js2.0 project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,234 @@
+<template>
+  <div id="app">
+    <img src="./assets/diyiping.png">
+    <!-- <hello></hello> -->
+    <div class="page2">
+      <div class="center">
+        <div class="left">
+          <img src="./assets/dierping.png">
+        </div>
+        <div class="right">
+          <p class="name">猪队友 <strong>主页</strong> </p>
+          <p class="describe">与小猪杀入战场吧</p>
+          <p class="text">"猪队友" 是一款多功能社交开黑平台。玩家可以在右上角个人中心包装自己。</p>
+          <p class="text">添加个性标签，上线匹配开黑队友或直接多人开黑。点击小猪上线与好友一起享受开黑乐趣吧。</p>
+        </div>
+      </div>
+    </div>
+    <div class="page3">
+      <div class="center">
+        <img src="./assets/disanping-tu.png">
+        <div class="right">
+          <p class="name">猪队友 <strong>主页</strong> </p>
+          <p class="describe">与小猪杀入战场吧</p>
+          <p class="text">"猪队友" 是一款多功能社交开黑平台。玩家可以在右上角个人中心包装自己。</p>
+          <p class="text">添加个性标签，上线匹配开黑队友或直接多人开黑。点击小猪上线与好友一起享受开黑乐趣吧。</p>
+        </div>
+      </div>
+    </div>
+    <div class="page4">
+      <div class="center">
+        <p class="name">猪队友 <strong>陪玩</strong></p>
+        <p class="describe">找陪玩或是当陪玩一切随心而变</p>
+        <p class="text">一个人玩游戏是不是有些寂寞?<br>四缺一是不是有些无奈说不出?<br>找些志同道合的人一起开黑吧!</p>
+      </div>
+      <div class="carousel"></div>
+    </div>
+    <div class="page5">
+      <div class="center">
+        <div class="left">
+          <p class="name">猪队友 <strong>认证</strong></p>
+          <p class="describe">认证注册向大家展现幕布后精彩的你</p>
+          <p class="text">妹子/大神在上传完资料后<br>我们会在第一时间由后台人工审核。<br>审核成功后会有独特的认证标志<br>并且可以成为陪玩自由定价开始接单啦。</p>
+        </div>
+        <img src="./assets/diwuping.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+    // Hello
+  }
+}
+</script>
+
+<style lang = "less">
+#app {
+  min-width: 1200px;
+  font-family:Arial,Helvetica;
+  img{
+    width: 100%;
+  }
+  .page2{
+    height:520px;
+    .center{
+      width: 965px;
+      margin: 0 auto;
+    }
+    .left{
+      width: 380px;
+      height: 100%;
+      display: inline-block;
+      /*border: 1px solid black;*/
+      img{
+        width: 100%;
+        margin: 0 auto;
+        vertical-align:-130px;
+      }
+    }
+    .right{
+      width: 432px;
+      margin-left: 144px;
+      display: inline-block;
+      /*border: 1px solid black;*/
+      .name{
+        font-size: 26px;
+        color: #956adc;
+        border-bottom: 1px solid #e7e7e7;
+        width: 158px;
+        margin: 0;
+      }
+      .describe{
+        font-size: 20px;
+        color: #353535;
+        margin-top: 10px;
+      }
+      .text{
+        font-size: 16px;
+        color: #6d6d6d;
+      }
+    }
+  }
+  .page3{
+    height: 753px;
+    /*border:1px solid black;*/
+    background: url("./assets/disanping-sanjiao.png") no-repeat 30px 0px;
+    background-size: 1264px;
+    .center{
+      width: 1004px;
+      height: 410px;
+      background-color: white;
+      padding-left: 156px;
+      /*border:1px solid black;*/
+      margin: 0 auto;
+      margin-top: 115px;
+      -moz-box-shadow:0px 2px 17px rgba(221, 196, 242, 0.5); 
+      -webkit-box-shadow:0px 2px 17px rgba(221, 196, 242, 0.5); 
+      box-shadow:0px 2px 17px rgba(221, 196, 242, 0.5);
+      img{
+        width: 270px;
+        margin-top: -88px;
+        vertical-align:-110px;
+      }
+      .right{
+        width: 432px;
+        margin-left: 144px;
+        display: inline-block;
+        /*border: 1px solid black;*/
+        .name{
+          font-size: 26px;
+          color: #956adc;
+          border-bottom: 1px solid #e7e7e7;
+          width: 158px;
+          margin: 0;
+        }
+        .describe{
+          font-size: 20px;
+          color: #353535;
+          margin-top: 10px;
+        }
+        .text{
+          font-size: 16px;
+          color: #6d6d6d;
+        }
+      }
+    }
+  }
+  .page4{
+    height: 760px;
+    border:1px solid black;
+    .center{
+      text-align: center;
+      width: 283px;
+      margin: 0 auto;
+      .name{
+        font-size: 26px;
+        color: #956adc;
+        border-bottom: 1px solid #e7e7e7;
+        width: 212px;
+        margin: 0 auto;
+        margin-top: 50px;
+        text-align: center;
+      }
+      .describe{
+        font-size: 20px;
+        color: #353535;
+        margin-top: 10px;
+      }
+      .text{
+        font-size: 16px;
+        color: #6d6d6d;
+      }
+    }
+    .carousel{
+      border:1px solid black;
+      width: 666px;
+      height: 480px;
+      margin: 0 auto;
+    }
+  }
+  .page5{
+    height: 770px;
+    border:1px solid black;
+    .center{
+      text-align: center;
+      width: 1070px;
+      /*height: 344px;*/
+      /*border:2px solid red;*/
+      margin: 0 auto;
+      margin-top: 126px;
+      .left{
+        width: 630px;
+        height: 340px;
+        border:10px solid #cacaca;
+        border-right: none;
+        background-color: #9668df;
+        display: inline-block;
+        /*padding-top: 55px;*/
+        .name{
+          font-size: 26px;
+          color: white;
+          border-bottom: 1px solid #e7e7e7;
+          width: 158px;
+          margin: 0 auto;
+          margin-top: 55px;
+        }
+        .describe{
+          font-size: 20px;
+          color: white;
+          margin-top: 10px;
+        }
+        .text{
+          font-size: 16px;
+          color: white;
+          line-height: 30px;
+        }
+      }
+      img{
+        display: inline-block;
+        width: 446px;
+        margin-left: -21px;
+        vertical-align: -155px;
+      }
+    }
+
+  }
+
+}
+</style>
